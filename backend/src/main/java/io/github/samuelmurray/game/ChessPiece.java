7ac622a6935cd118f0338e5da63005592983b026
Samuel Murray <samuel.murray@outlook.com>
2020-04-25 19:49:27 +0200

    Move shared logic from King to ChessPiece

--- backend/src/main/java/io/github/samuelmurray/game/ChessPiece.java
+++ backend/src/main/java/io/github/samuelmurray/game/ChessPiece.java
@@ -1,7 +1,9 @@
 package io.github.samuelmurray.game;
 
 
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public abstract class ChessPiece {
     protected final Team team;
@@ -10,5 +12,16 @@ public abstract class ChessPiece {
         this.team = team;
     }
 
-    public abstract Set<Position> getValidMoves(GameState gameState);
+    public Set<Position> getValidMoves(GameState gameState) {
+        Position currentPosition = gameState.getPositionOfPiece(this)
+                .orElseThrow(() -> new ChessException("Can't call getValidModes on piece missing from the game state"));
+        return getPotentiallyValidMoves(currentPosition).stream()
+                .filter(position -> {
+                    Optional<ChessPiece> maybeOther = gameState.getPieceAt(position);
+                    return maybeOther.map(piece -> piece.team != this.team).orElse(true);
+                })
+                .collect(Collectors.toSet());
+    }
+
+    protected abstract Set<Position> getPotentiallyValidMoves(Position currentPosition);
 }
--- backend/src/main/java/io/github/samuelmurray/game/King.java
+++ backend/src/main/java/io/github/samuelmurray/game/King.java
@@ -1,6 +1,5 @@
 package io.github.samuelmurray.game;
 
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,18 +11,7 @@ public class King extends ChessPiece {
     }
 
     @Override
-    public Set<Position> getValidMoves(GameState gameState) {
-        Position currentPosition = gameState.getPositionOfPiece(this)
-                .orElseThrow(() -> new ChessException("Can't call getValidModes on piece missing from the game state"));
-        return getPotentiallyValidMoves(currentPosition).stream()
-                .filter(position -> {
-                    Optional<ChessPiece> maybeOther = gameState.getPieceAt(position);
-                    return maybeOther.map(piece -> piece.team != this.team).orElse(true);
-                })
-                .collect(Collectors.toSet());
-    }
-
-    private static Set<Position> getPotentiallyValidMoves(Position currentPosition) {
+    protected Set<Position> getPotentiallyValidMoves(Position currentPosition) {
         return Stream.of(currentPosition.above, currentPosition.below, currentPosition.left, currentPosition.right,
                 currentPosition.above.right, currentPosition.above.left, currentPosition.below.right, currentPosition.below.left)
                 .filter(position -> position != Position.OUT_OF_BOARD)
