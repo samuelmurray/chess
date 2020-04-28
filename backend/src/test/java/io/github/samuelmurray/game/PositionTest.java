02596bc667760b98cf8422be736d0eed59cc16d1
Samuel Murray <samuelmu@kth.se>
2020-04-28 21:02:25 +0200

    Bugfix: Check for OUT_OF_BOARD instead of null

--- backend/src/test/java/io/github/samuelmurray/game/PositionTest.java
+++ backend/src/test/java/io/github/samuelmurray/game/PositionTest.java
@@ -2,6 +2,8 @@ package io.github.samuelmurray.game;
 
 import org.junit.jupiter.api.Test;
 
+import static io.github.samuelmurray.game.Position.*;
+import static io.github.samuelmurray.game.Position.A1;
 import static io.github.samuelmurray.game.Position.OUT_OF_BOARD;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,47 +13,47 @@ class PositionTest {
 
     @Test
     void testPositionHas65Entries() {
-        assertEquals(65, Position.values().length);
+        assertEquals(65, values().length);
     }
 
     @Test
     void testA1IsBelowB1() {
-        assertEquals(Position.A1, Position.B1.getBelow());
+        assertEquals(A1, B1.getBelow());
     }
 
     @Test
     void testB1IsAboveA1() {
-        assertEquals(Position.B1, Position.A1.getAbove());
+        assertEquals(B1, A1.getAbove());
     }
 
     @Test
     void testA2IsRightOfA1() {
-        assertEquals(Position.A2, Position.A1.getRight());
+        assertEquals(A2, A1.getRight());
     }
 
     @Test
     void testA1IsLeftOfA2() {
-        assertEquals(Position.A1, Position.A2.getLeft());
+        assertEquals(A1, A2.getLeft());
     }
 
     @Test
     void testA1HasNothingBelow() {
-        assertNull(Position.A1.getBelow());
+        assertEquals(OUT_OF_BOARD, A1.getBelow());
     }
 
     @Test
     void testA1HasNothingLeft() {
-        assertNull(Position.A1.getLeft());
+        assertEquals(OUT_OF_BOARD, A1.getLeft());
     }
 
     @Test
     void testH8HasNothingAbove() {
-        assertNull(Position.H8.getAbove());
+        assertEquals(OUT_OF_BOARD, H8.getAbove());
     }
 
     @Test
     void testH8HasNothingRight() {
-        assertNull(Position.H8.getRight());
+        assertEquals(OUT_OF_BOARD, H8.getRight());
     }
 
     @Test
@@ -66,7 +68,7 @@ class PositionTest {
 
     @Test
     void testRelationsAreAntiSymmetrical() {
-        for (var position : Position.values()) {
+        for (var position : values()) {
             if (position.getAbove() != null && position.getAbove() != OUT_OF_BOARD) {
                 assertEquals(position, position.getAbove().getBelow());
             }
